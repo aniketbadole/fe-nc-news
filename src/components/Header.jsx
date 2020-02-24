95aed7bbbb8f672d0713e71fcd9989d3f46b64ea
Aniket <aniket.b@outlook.com>
2020-02-24 14:05:22 +0000

    creating react structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return (
+      <div class="App-header">
+        <span>^</span>NCReddit
+      </div>
+    );
+  }
+}
+
+export default Header;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Nav extends Component {
+  render() {
+    return (
+      <nav>
+        <ol>
+          <li>Home</li>
+          <li>Articles</li>
+          <li>Topics</li>
+          <li>Something else</li>
+        </ol>
+      </nav>
+    );
+  }
+}
+
+export default Nav;
